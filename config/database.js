0e96edf46d7ccc920dfaf08170bb2f03958c002a
rem317 <rembenares3@gmail.com>
2026-02-24 03:44:33 +0800

    Added SSL for TiDB connection

--- config/database.js
+++ config/database.js
@@ -6,7 +6,12 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 4000,
+  // I-add itong ssl section
+  ssl: {
+    rejectUnauthorized: true,
+    minVersion: 'TLSv1.2'
+  }
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
